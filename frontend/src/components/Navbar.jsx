4c8329a2448434466dbbf9ddeb3239c658182dee
Monalisa Biswal <monalisaabiswall@gmail.com>
2026-04-04 21:41:32 +0530

    responsive navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '/proofit-logo.svg'
 import { Link } from 'react-router-dom'
 import Button from './Button'
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
       <div className="logo">
-        <Link to="/"><img src={logo} alt="Proofit Logo" width={35} height={35} />
+        <Link to="/">
+          <img src={logo} alt="Proofit Logo" width={35} height={35} />
         </Link>
         <span className='site-name'>Proofit</span>
       </div>
-      <ul className="nav-links">
+
+      {/* Burger Icon */}
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        <i className={menuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li><Link to="/verify">Verify</Link></li>
         <li><Link to="/how-it-works">How it works</Link></li>
         <li><Link to="/about">About</Link></li>
       </ul>
+
       <div className="cta">
         <Link to="/analyze"><Button /></Link>
       </div>
