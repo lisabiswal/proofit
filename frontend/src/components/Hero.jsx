73da3235d9f0d12d5d3c9034e29146a3863476ff
Monalisa Biswal <monalisaabiswall@gmail.com>
2026-04-06 22:03:45 +0530

    improve thr design of hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,18 +4,20 @@ import Button from './Button'
 import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="left">
-        <h1>Detect Fraud. <br /> Verify Authenticity</h1>
-        <p>Check if online content is real or fake in seconds. Our AI-powered platform analyzes text, news, links, and documents to protect you from misinformation</p>
-        <div className="cta-hero">
-          <Link to="/analyze"><Button /></Link>
+    <div className="container">
+      <div className="hero">
+        <div className="left">
+          <h1>Detect <span className='fraud'>Fraud.</span> <br /> Verify <span className='auth'>Authenticity</span></h1>
+          <p>Check if online content is real or fake in seconds. Our AI-powered platform analyzes text, news, links, and documents to protect you from misinformation</p>
+          <div className="cta-hero">
+            <Link to="/analyze"><Button /></Link>
+          </div>
         </div>
-      </div>
-      <div className="right">
-        <img src="proofit-hero-pic.jpg" alt="hero image" width={500} height={400} />
-        <div className="verify-badge">
-          <p>Verified</p>
+        <div className="right">
+          <img src="proofit-hero-pic.jpg" alt="hero image" width={400} height={400} />
+          <div className="verify-badge">
+            <p>Verified</p>
+          </div>
         </div>
       </div>
     </div>
